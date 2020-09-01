c934fccf58d42649d7121e93b4101217e338c06c
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-09-01 19:10:08 +0200

    fix: remove unused google fonts styles

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,10 +3,22 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
 import process from 'process'
 
-const links = [
+const googleFonts = (fonts) =>
+	`https://fonts.googleapis.com/css2?family=${fonts}&display=swap`
+
+const devLinks = [
 	{
-		href:
-			'https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap',
+		href: googleFonts('Space+Mono:ital,wght@0,400;0,700;1,400;1,700'),
+	},
+	{
+		href: 'https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css',
+		integrity: 'sha256-WAgYcAck1C1/zEl5sBl5cfyhxtLgKGdpI3oKyJffVRI=',
+	},
+]
+
+const prodLinks = [
+	{
+		href: googleFonts('Space+Mono:wght@400;700'),
 	},
 	{
 		href: 'https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css',
@@ -15,6 +27,7 @@ const links = [
 ]
 
 const isDev = process.env.NODE_ENV === 'development'
+const links = isDev ? devLinks : prodLinks
 
 class Doc extends Document {
 	static async getInitialProps(context) {
