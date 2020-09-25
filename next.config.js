d6045573d4d73cf785ab8aa0583563deda5125cb
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-09-25 20:02:52 +0200

    fix: remove csp header from next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withAssetsManifest = require('next-assets-manifest')
 const isProd =
 	process.env.NODE_ENV === 'production' && process.env.APP_ENV !== 'staging'
 
-const nonce = process.env.CSP_NONCE
+// const nonce = process.env.CSP_NONCE
 
 module.exports = withAssetsManifest({
 	reactStrictMode: false,
@@ -19,19 +19,6 @@ module.exports = withAssetsManifest({
 		})
 
 		return config
-	},
-	async headers() {
-		return [
-			{
-				source: '/(.*)',
-				headers: [
-					{
-						key: 'Content-Security-Policy',
-						value: `default-src 'self' cdn.bratteng.sh; style-src 'nonce-${nonce}' fonts.googleapis.com; font-src fonts.gstatic.com; script-src 'nonce-${nonce}'`,
-					},
-				],
-			},
-		]
 	},
 	// Customize the client side manifest.
 	assetsManifestClient: {
