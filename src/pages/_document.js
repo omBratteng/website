77a73a15d9d9031497a7528adfadd456f687a3c7
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-11-24 15:33:00 +0100

    Fix HTML for preload links

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,8 @@ import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
 import process from 'process'
 import { parse as URLParse } from 'url'
 
+import PropTypes from 'prop-types'
+
 import { getCookie } from 'hooks/useCookie'
 
 const googleFonts = (fonts) =>
@@ -99,44 +101,73 @@ class Doc extends Document {
 	}
 }
 
-const PreConnect = ({ hrefs }) =>
-	hrefs.map((href, key) => (
-		<link rel="preconnect" href={href} key={key} crossOrigin="anonymous" />
-	))
-
 const PreloadStyles = ({ links }) => {
 	let preconnect = new Set()
 	let preload = new Set()
 	let stylesheet = new Set()
-	links.map((linkProps) => {
-		let url = new URLParse(linkProps.href)
+	let scripts = new Set()
+
+	links.map((linkProps, key) => {
+		const { as, integrity, href, autoload = true } = linkProps
+		const url = URLParse(href)
+
 		preconnect.add(`${url.protocol}//${url.host}`)
 
 		if (url.pathname !== '/') {
 			preload.add(
 				<link
+					key={key}
 					rel="preload"
-					as="style"
-					{...linkProps}
+					as={as}
+					integrity={integrity}
 					crossOrigin="anonymous"
+					{...linkProps}
 				/>,
 			)
-			stylesheet.add(
-				<link
-					rel="stylesheet"
-					{...linkProps}
-					crossOrigin="anonymous"
-					async
-				/>,
-			)
+
+			if (!autoload) return
+
+			as === 'style'
+				? stylesheet.add(
+						<link
+							key={key}
+							rel="stylesheet"
+							href={linkProps.href}
+							crossOrigin="anonymous"
+						/>,
+				  )
+				: scripts.add(
+						<script
+							key={key}
+							src={linkProps.href}
+							crossOrigin="anonymous"
+							async
+						/>,
+				  )
 		}
 	})
 
-	return [
-		<PreConnect hrefs={[...preconnect]} key="0" />,
-		...preload,
-		...stylesheet,
-	]
+	return (
+		<>
+			{[
+				[...preconnect].map((href, key) => (
+					<link
+						rel="preconnect"
+						href={href}
+						key={key}
+						crossOrigin="anonymous"
+					/>
+				)),
+				...preload,
+				...stylesheet,
+				...scripts,
+			]}
+		</>
+	)
+}
+
+PreloadStyles.propTypes = {
+	links: PropTypes.array,
 }
 
 export default Doc
