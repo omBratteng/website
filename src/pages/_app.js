35f20cbed7b5e07982dc2f9d3952baa50ed65051
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-12-05 14:51:32 +0100

    style: cleanup FontAwesome imports

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,30 +2,19 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-
-// import { fab } from '@fortawesome/free-brands-svg-icons'
-// import { fad } from '@fortawesome/pro-duotone-svg-icons'
-// import { fal } from '@fortawesome/pro-light-svg-icons'
-// import { fas } from '@fortawesome/pro-solid-svg-icons'
-// import { far } from '@fortawesome/pro-regular-svg-icons'
-// library.add(far, fad)
 import { faHashtag } from '@fortawesome/pro-regular-svg-icons'
-library.add(faHashtag)
 
 import {
 	faGithubAlt,
 	faTwitter,
 	faLinkedinIn,
 } from '@fortawesome/free-brands-svg-icons'
-library.add(faGithubAlt, faTwitter, faLinkedinIn)
 
 import {
 	faLightbulb,
 	faLightbulbOn,
 	faLightbulbSlash,
 } from '@fortawesome/pro-duotone-svg-icons'
-
-library.add(faLightbulb, faLightbulbOn, faLightbulbSlash)
 
 // Next.js
 import Head from 'next/head'
@@ -36,6 +25,10 @@ import AppProvider from 'contexts/app'
 // Components
 import Layout from 'components/layout/Layout'
 import useAnalytics from 'hooks/useAnalytics'
+
+library.add(faHashtag)
+library.add(faGithubAlt, faTwitter, faLinkedinIn)
+library.add(faLightbulb, faLightbulbOn, faLightbulbSlash)
 
 const App = ({ Component, pageProps }) => {
 	useAnalytics({
