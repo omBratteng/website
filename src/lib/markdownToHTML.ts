941c7feafabdf6af4befecd4506b0bc68e4a8c58
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-12-15 06:04:09 +0100

    fix: remove last remnant of blog

--- src/lib/markdownToHTML.ts
+++
@@ -1,9 +0,0 @@
-import remark from 'remark'
-import html from 'remark-html'
-
-const markdownToHTML = async (markdown: string): Promise<string> => {
-	const result = await remark().use(html).process(markdown)
-	return result.toString()
-}
-
-export default markdownToHTML
