8cf41d1de45a668231efa68ab1976d8d506b457c
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2021-01-11 20:45:36 +0100

    test: setup jest and dependencies

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	roots: ['<rootDir>/'],
+	preset: 'ts-jest',
+	resetMocks: true,
+	moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'jsx'],
+	testPathIgnorePatterns: ['<rootDir>[/\\\\](node_modules|.next)[/\\\\]'],
+	transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$'],
+	transform: {
+		'^.+\\.(ts|tsx)$': 'babel-jest',
+	},
+	moduleDirectories: ['node_modules', 'src'],
+	watchPlugins: [
+		'jest-watch-typeahead/filename',
+		'jest-watch-typeahead/testname',
+	],
+	moduleNameMapper: {
+		'\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+		'\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+	},
+	setupFilesAfterEnv: ['./test/jest.setup.ts'],
+}
