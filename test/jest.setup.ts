bda73aee024e155d93bd41ae013c054f6e58f61d
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2021-01-11 21:11:56 +0100

    test: add FontAwesome icons to jest setup

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,2 +1,9 @@
 import 'jest-styled-components'
 import '@testing-library/jest-dom/extend-expect'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { far } from '@fortawesome/pro-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fad } from '@fortawesome/pro-duotone-svg-icons'
+
+library.add(far, fab, fad)
