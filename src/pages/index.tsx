62249f3d7d47f75cc2dd854c20939ec8d3ede389
Ole-Martin Bratteng <ole-martin@bratteng.com>
2021-05-29 03:39:15 +0200

    fix: use https links on index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,14 +17,14 @@ const Index = (): JSX.Element => {
 	return (
 		<>
 			<Section title="About">
-				<SectionContent itemScope itemType="http://schema.org/Person">
+				<SectionContent itemScope itemType="https://schema.org/Person">
 					<span itemProp="name">Ole-Martin Bratteng</span> &mdash; Software Developer. Located in the beautiful city
 					of{' '}
-					<a href="http://en.wikipedia.org/wiki/Trondheim">
+					<a href="https://en.wikipedia.org/wiki/Trondheim">
 						<span itemProp="address">Trondheim</span>, <span itemProp="nationality">Norway</span>
 					</a>
 					. <span itemProp="jobTitle">Software Developer</span> at{' '}
-					<span itemProp="worksFor" itemScope itemType="http://schema.org/Organization">
+					<span itemProp="worksFor" itemScope itemType="https://schema.org/Organization">
 						<a href="https://www.arkivverket.no/en" itemProp="url">
 							<span itemProp="name">the National Archives of Norway</span>
 						</a>
