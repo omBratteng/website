f4e2f2bf6be8a19ecea313a7f20ffb02d342a5ac
Ole-Martin Bratteng <ole-martin@bratteng.com>
2021-09-12 04:03:02 +0200

    feat: allow to manually purge redis cache

--- src/pages/api/offsetTonnes.ts
+++ src/pages/api/offsetTonnes.ts
@@ -5,10 +5,10 @@ import fetchWrenOffset from 'utils/fetchWrenOffset'
 
 import redis from 'lib/redis'
 
-const ApiEndpoint: NextApiHandler<OffsetTonnes> = async (_, res) => {
+const ApiEndpoint: NextApiHandler<OffsetTonnes> = async ({ method }, res) => {
 	res.setHeader('Cache-Control', 'public, max-age=604800')
 
-	const cache = await redis.get('offsetTonnes')
+	const cache = method === 'PURGE' ? null : await redis.get('offsetTonnes')
 	const offsetTonnes: OffsetTonnes = cache ? JSON.parse(cache) : await fetchWrenOffset()
 
 	!cache && (await redis.set('offsetTonnes', JSON.stringify(offsetTonnes), 'EX', '604800'))
