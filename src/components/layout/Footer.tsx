02c6af6070654b59069bf07c97f0afa93a867537
Ole-Martin Bratteng <ole-martin@bratteng.com>
2021-09-16 12:49:49 +0200

    refactor: remove SWR to load offsetTonnes from frontend

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components'
-import useSWR from 'swr'
 
 import { OffsetTonnes } from 'types/dto'
 
@@ -22,12 +21,10 @@ type Props = {
 }
 
 const Footer = ({ offset }: Props): JSX.Element => {
-	const { data } = useSWR<OffsetTonnes>('/api/offsetTonnes', { fallbackData: offset })
-
 	return (
 		<>
 			<StyledFooter>
-				So far I have offset a total of {data?.offsetTonnes ?? '00.00'} tons of CO
+				So far I have offset a total of {offset.offsetTonnes ?? '00.00'} tons of CO
 				<sub>2</sub> with{' '}
 				<a href="https://www.wren.co/join/omBratteng?utm_campaign=share&utm_medium=profile_referral_link">
 					Project Wren
--- src/contexts/app.tsx
+++ src/contexts/app.tsx
@@ -1,6 +1,5 @@
 import { createContext } from 'react'
 import { ThemeProvider } from 'styled-components'
-import { SWRConfig } from 'swr'
 
 // Next.js
 import Head from 'next/head'
@@ -8,7 +7,6 @@ import Head from 'next/head'
 import { GlobalStyle, dark, light } from 'styles'
 
 import useTheme, { IUseTheme } from 'hooks/useTheme'
-import { fetcher } from 'utils'
 
 export interface AppContextProps extends IUseTheme {
 	siteTitle: string
@@ -30,12 +28,10 @@ const AppProvider = ({ siteTitle, children }: Props): JSX.Element => {
 				<title>{siteTitle}</title>
 			</Head>
 			<AppContext.Provider value={{ siteTitle, theme, setTheme }}>
-				<SWRConfig value={{ fetcher }}>
-					<ThemeProvider theme={theme ? dark : light}>
-						<GlobalStyle />
-						{children}
-					</ThemeProvider>
-				</SWRConfig>
+				<ThemeProvider theme={theme ? dark : light}>
+					<GlobalStyle />
+					{children}
+				</ThemeProvider>
 			</AppContext.Provider>
 		</>
 	)
