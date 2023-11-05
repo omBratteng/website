630a19861927e4c3a695d088239415f0294370d2
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2023-11-05 03:23:20 +0100

    chore: drop healthcheck endpoint

--- src/pages/api/_health.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiHandler } from 'next'
-
-const Health: NextApiHandler = (_, res) =>
-	res.status(200).json({
-		status: 'OK',
-		time: new Date().toISOString(),
-	})
-
-export default Health
