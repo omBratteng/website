a9228a05691c05e695175c6daa2b11e8a0f823ec
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2024-12-01 01:02:41 +0100

    feat: atproto-did file

---
+++ next.config.ts
@@ -0,0 +1,56 @@
+import type { NextConfig } from 'next'
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+// 	enabled: process.env.ANALYZE === 'true',
+// })
+
+const isProd = process.env.NODE_ENV === 'production' && process.env.APP_ENV !== 'staging'
+
+const assetPrefix = isProd ? 'https://cdn.bratteng.com' : ''
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	// output: 'standalone',
+	reactStrictMode: false,
+	poweredByHeader: false,
+	...(isProd && { assetPrefix }),
+	serverRuntimeConfig: {},
+	publicRuntimeConfig: {
+		assetPrefix,
+	},
+	headers: async () => [
+		{
+			source: '/.well-known/atproto-did',
+			headers: [
+				{
+					key: 'Content-Type',
+					value: 'text/plain',
+				},
+			],
+		},
+		{
+			source: '/(.*)',
+			headers: [
+				{
+					key: 'X-Frame-Options',
+					value: 'DENY',
+				},
+				{
+					key: 'X-Content-Type-Options',
+					value: 'nosniff',
+				},
+				{
+					key: 'Referrer-Policy',
+					value: 'strict-origin-when-cross-origin',
+				},
+				{
+					key: 'X-XSS-Protection',
+					value: '1; mode=block',
+				},
+			],
+		},
+	],
+} satisfies NextConfig
+
+export default nextConfig
