698b7e534256ed6d3036d2489f399225de2b2a75
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2026-07-21 11:12:24 +0200

    fix: decode punycode

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -1,11 +1,12 @@
 import { headers } from 'next/headers'
+import { domainToUnicode } from 'node:url'
 import type { ReactElement } from 'react'
 
 import { PageTitleLink } from 'components/PageTitleLink'
 
 const PageTitle = async (): Promise<ReactElement> => {
 	const host = (await headers()).get('host') ?? 'bratteng.com'
-	const hostname = host.split(':')[0]
+	const hostname = domainToUnicode(host.split(':')[0])
 
 	return (
 		<h1 className="text-4xl pb-4 font-normal text-center w-full before:content-['~/'] before:text-(--red)">
